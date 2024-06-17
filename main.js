587d74128d272fa6423fc74a2fb90e528ef83cae
GuilhermeWeiber <160506709+GuilhermeWeiber@users.noreply.github.com>
2024-06-17 11:47:44 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -71,5 +71,9 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('Forte');
     } else if (entropia > 35 && entropia < 57 ) {
         forcaSenha.classList.add('media');
+    } else if (entropia <= 35){
+        forcaSenha.classList.add('fraca');
     }
+    const valorEntropia = document.querySelector('.entropia');
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(10))
 }
